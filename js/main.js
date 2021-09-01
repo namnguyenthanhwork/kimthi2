aa37a947b4458b154bbc12ff993fa1fa45a41757
namnguyenthanhwork <78526193+namnguyenthanhwork@users.noreply.github.com>
2021-09-01 20:21:59 +0700

    fix

--- js/main.js
+++ js/main.js
@@ -95,7 +95,7 @@ const app = {
             singer: "Alec Benjamin",
             path: "./music/Let Me Down Slowly- Alec Benjamin.mp3",
             image: "./img/Capture10.jpg"
-        }, 
+        },
         {
             name: "Love Me Like That",
             singer: "Sam Kim",
